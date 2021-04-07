e067c370c44dfa533a421b8c47a0713b501735dd
eduardosalaz <eduardosalaz@outlook.com>
2021-04-07 15:12:11 -0500

    Comentarios y cambio de nombres de variables

--- pia/constructive.jl
+++ pia/constructive.jl
@@ -2,18 +2,18 @@ try
     file = ARGS[1]
     stream = open(file, "r")
     contents = read(stream, String)
-    numLocations, distances, flow = split(contents, "\n")
+    numLocations, distString, flowString = split(contents, "\n") # cada salto de linea representa un valor nuevo
     numLocations = parse(Int, numLocations)
-    distancias = [parse(Int, distancia) for distancia in split(distances)]
-    distancias = reshape(distancias, (numLocations, numLocations))
-    flujos = [parse(Int, flujo) for flujo in split(flow)]
-    flujos = reshape(flujos, (numLocations, numLocations))
+    distances = [parse(Int, distance) for distance in split(distString)] # pasamos el string que leimos a un arreglo 
+    distanceM = reshape(distances, (numLocations, numLocations)) # al arreglo le damos la forma de la matriz tomando en cuenta numLocations 
+    flows = [parse(Int, flow) for flow in split(flowString)]
+    flowM = reshape(flows, (numLocations, numLocations))
     println("Number of Locations: " * string(numLocations, base=10))
     print("Distance matrix: ")
-    show(stdout, "text/plain", distancias)
+    show(stdout, "text/plain", distanceM)
     println()
     print("Flow matrix: ")
-    show(stdout, "text/plain", flujos)
+    show(stdout, "text/plain", flowM)
     println()
 catch
     @error "File is invalid"
--- pia/generator.jl
+++ pia/generator.jl
@@ -13,46 +13,47 @@ Crea también una matriz de flujo aleatoria entre estas localizaciones
 Guarda ambas matrices en un .dat para su posterior lectura
 """
 function generate(numLocations, instance)
-    minimo = rand(1:20)
-    maximo = rand(80:100)
-    coords = rand(minimo:maximo, (numLocations, 2)) # primer valor representa x, segundo valor representa y
+    minimal = rand(1:20) # numero aleatorio del rango del 1 al 20
+    maximal = rand(80:100) # del 80 al 100
+    coords = rand(minimal:maximal, (numLocations, 2)) # primer valor representa x, segundo valor representa y
     # cada fila representa una ciudad
     print("Coordinates: ")
     show(stdout, "text/plain", coords)
-    dist = Euclidean()
-    distancias = pairwise(dist, coords, dims=1) #el dims=1 es para especificar que vamos por las FILAS
-    distancias = trunc.(Int, distancias) #las pasamos a enteros, el . es que se broadcastea a toda la matriz
-    flujos = rand(minimo:maximo, (numLocations, numLocations))
-    flujos[diagind(flujos)] .= 0 #convierte los índices de la diagonal principal de flujos en 0s
-    flujos = Symmetric(flujos) #agarra la parte superior y la aplica a la parte inferior de la matriz
+    euclides = Euclidean() # necesitamos calcular distancias Euclidianas
+    distances = pairwise(euclides, coords, dims=1) # el dims=1 es para especificar que vamos por las FILAS
+    distances = trunc.(Int, distances) # las pasamos a enteros, el . es que se broadcastea a toda la matriz
+    flow = rand(minimal:maximal, (numLocations, numLocations)) # genera una matriz de randoms del rango de n×n filas
+    flow[diagind(flow)] .= 0 # convierte los índices de la diagonal principal de flujos en 0s
+    flow = Symmetric(flow) # agarra la parte superior y la aplica a la parte inferior de la matriz, volviendola simetrica
     println()
     print("Distance matrix: ")
-    show(stdout, "text/plain", distancias)
+    show(stdout, "text/plain", distances)
     println()
     print("Flow matrix: ")
-    show(stdout, "text/plain", flujos)
-    name = "file" * string(instance, base=10)
-    x = coords[:,1]
-    y = coords[:,2]
+    show(stdout, "text/plain", flow)
+    name = "file" * string(instance, base=10) # nombre de archivo de instancia actual
+    # EN JULIA EL OPERADOR * ES PARA CONCATENAR STRINGS
+    x = coords[:,1] # agarra todas las filas de la primera columna 
+    y = coords[:,2] # agarra todas las filas de la segunda columna
     p = plot(x=x,y=y, Geom.point, Theme(panel_fill=colorant"white", default_color=colorant"orange"));
     push!(p, Guide.title("Locations"));
-    name_plot = name * ".pdf"
-    draw(PDF(name_plot, 5inch, 5inch), p)
+    name_plot = name * ".pdf" # en png se cambian los colores no se muy bien porque
+    draw(PDF(name_plot, 5inch, 5inch), p) # se guarda el plot
     name = name * ".dat"
     firstLine = string(numLocations, base=10) * "\n"
     open(name, "w") do io
         write(io, firstLine)
-        writedlm(io, [distancias], ' ')
-        writedlm(io, [flujos], ' ')
-    end
+        writedlm(io, [distances], ' ')
+        writedlm(io, [flow], ' ')
+    end # escribimos al archivo nuestro numero de localizaciones, matrices de distancia y flujo
     println()
     printstyled(stdout, "Files written\n", color=:green)
 end
 
 try
-    numLocations = parse(Int64, ARGS[1])
+    numLocations = parse(Int64, ARGS[1]) #ARGS es el arreglo de argumentos del programa
     numInstances = parse(Int64, ARGS[2])
-    dir_path = string(numLocations, base=10)
+    dir_path = string(numLocations, base=10) #creamos el nombre del directorio en el que se guardaran las instancias
     if !isdir(dir_path) #if dir doesnt exist yet
         mkdir(dir_path)
     end
