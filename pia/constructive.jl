dbd05d98b556ee5b7327f6223f5811402cd3f5c6
eduardosalaz <eduardosalaz@outlook.com>
2021-04-09 17:24:52 -0500

    Para guardar solucion en archivo, todavia wip

--- pia/constructive.jl
+++ pia/constructive.jl
@@ -1,4 +1,4 @@
-using SparseArrays
+using SparseArrays, DelimitedFiles
 
 # pseudocódigo del algoritmo
 # 1.- Input: matriz de costos, número de iteración
@@ -11,7 +11,7 @@ using SparseArrays
 #   8.- número de iteración += 1
 # 9.- Return matriz de decisión, Σ
 
-function heuristic(cost, X, iter, sum)
+function heuristic(cost, X, iter, sum,verbose=false)
     value, index = findmin(cost) # análogo al paso 4
     x = index[1] # paso 4
     y = index[2] # paso 4
@@ -19,22 +19,32 @@ function heuristic(cost, X, iter, sum)
     cost[x,:] .= 9999999 # paso 5
     X[x,y] = 1 # paso 6
     sum += value # paso 7
-    printCost = sparse(cost) # todo esto es para imprimir la matriz de costos actualizada
-    replace!(printCost, 9999999=>0)
-    dropzeros!(printCost)
-    print("\nCheapest cost: ", value, " found at: ", x, ",", y)
-    print("\nDecision matrix: ")
-    show(stdout, "text/plain", X)
-    println("\nUpdated cost matrix: ")
-    show(stdout, "text/plain", printCost)
-    println()
+    if verbose
+        printCost = sparse(cost) # todo esto es para imprimir la matriz de costos actualizada
+        replace!(printCost, 9999999=>0)
+        dropzeros!(printCost)
+        print("\nCheapest cost: ", value, " found at: ", x, ",", y)
+        print("\nDecision matrix: ")
+        show(stdout, "text/plain", X)
+        println("\nUpdated cost matrix: ")
+        show(stdout, "text/plain", printCost)
+        println()
+    end
     iter += 1 # paso 8
     return cost,X,iter,sum # paso 9
 end
 
 try
     file = ARGS[1]
-    verbose = parse(Int, ARGS[2])
+    verbose = false
+    saveTo = false
+    argumentos = length(ARGS)
+    if argumentos > 1
+        verbose = true
+    elseif argumentos > 2
+        saveTo = true
+    end
+    
     stream = open(file, "r")
     contents = read(stream, String)
     numLocations, distString, flowString = split(contents, "\n") # cada salto de linea representa un valor nuevo
@@ -46,7 +56,7 @@ try
     × = * # mejor notación
     costM = distanceM × flowM # matriz de costos, paso 1
     iters = 0 # paso 1
-    if verbose == 1
+    if verbose
         println("Number of Locations: " * string(numLocations, base=10))
         print("\nDistance matrix: ")
         show(stdout, "text/plain", distanceM)
@@ -61,7 +71,7 @@ try
     X = zeros(Int8, numLocations, numLocations) # matriz de decisión, paso 2
     Σ = 0 # paso 2
     @time while iters < numLocations # paso 3
-        costM, X, iters,Σ = heuristic(costM, X, iters, Σ)
+        costM, X, iters,Σ = heuristic(costM, X, iters, Σ,verbose)
     end
     printstyled(stdout, "End of Heuristic\n", color=:green)
     println("Total cost: ", Σ)
@@ -74,7 +84,16 @@ try
         push!(localizaciones, x)
     end
     println("\nList of facilities: ", localizaciones)
-
+    if saveTo
+        name = "solution_" * string(numLocations, base=10) * ".dat"
+        firstline = "Cost: " * string(Σ, base=10) * "\n"
+        open(name, "w") do io
+            write(io, firstline)
+            writedlm(io, [localizaciones], ' ')
+            writedlm(io, [X], ' ')
+        end
+        printstyled(stdout, "Wrote to file\n", color=:green)
+    end
 catch
     @error "Invalid file or missing arguments"
 end
